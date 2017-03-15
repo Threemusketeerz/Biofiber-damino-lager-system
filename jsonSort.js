208263421a1da95f64be63950915d15059f9bc65
threemusketeerz <work.bjarke@gmail.com>
2017-03-15 15:06:25 +0100

    Revert "Fixed history bug. History now properly adds key:values else del itself"
    This reverts commit 823ccd960ea0b84f444c712d10bf40d1065b207e.

--- jsonSort.js
+++ jsonSort.js
@@ -1,6 +1,6 @@
 function jsonSorter(objArr) {
     var newObjArr = [];
-    // var obj;
+    var obj;
     var objKeys;
     objArr.sort(sortJsonDate);
 
@@ -10,7 +10,7 @@ function jsonSorter(objArr) {
     }
 
     for (var i = 0; i < objArr.length; i++) {
-        var obj = objArr[i];
+        obj = objArr[i];
         var curVal = objArr[i]["Lokation"];
         var curValD = objArr[i]["Dato"];
         newObjArr.push(obj);
@@ -18,17 +18,10 @@ function jsonSorter(objArr) {
         for (var j = 0; j < newObjArr.length; j++) {
             var newVal = newObjArr[j]["Lokation"];
             var newValD = newObjArr[j]["Dato"];
-            var hist = "Historik";
-            if (!(hist in newObjArr[j])) {
-                newObjArr[j][hist] = [];
-            }
-
             if (newVal === curVal && newValD !== curValD) {
-                newObjArr[j][hist].push(objArr[i]);
+                newObjArr[j]["Historik"] = [];
+                newObjArr[j]["Historik"].push(objArr[i]);
                 newObjArr.pop();
-            }
-            else if (newObjArr[j][hist].length === 0) {
-                delete newObjArr[j][hist];
             }
         }
     }
