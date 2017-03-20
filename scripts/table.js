9461ad05ce45f33e40577ee03eea7840d7974a30
Sporring55 <bj@damino02.local>
2017-03-20 11:51:25 +0100

    Update on click funtion + show function
    Tables that is hidden is now being done with hide(); function

--- scripts/table.js
+++ scripts/table.js
@@ -5,16 +5,19 @@ $.getJSON("scripts/results.json", function(data) {
 	console.log(finArr);
 	htmlTable(".container", finArr);
 	$("table").addClass("table table-responsive table-striped table-hover");
+	
 });
 /*All credit to agershun for the htmlTable() function,  
 *2nd answer on http://stackoverflow.com/questions/27681838/create-table-with-javascript-array-and-object
 */
+
 function htmlTable(selector, data, columns) {
 	var sel = document.querySelector(selector);
 	var tbe = document.createElement('table');
 	var thead = document.createElement('thead');
 	var tre = document.createElement('tr');
 	var tbody = document.createElement('tbody');
+	var Class = "clicked";
 
 	if(!sel) {
 		throw new Error('Selected HTML element is not found');
@@ -35,35 +38,98 @@ function htmlTable(selector, data, columns) {
 
 	tbe.appendChild(tbody);
 	for (var j = 0; j < data.length; j++){
+		var d = data[j]["Historik"];
 		var tre = document.createElement('tr');
+			tre.id = "log" + j;
 		for (var i = 0; i < columns.length - 1; i++){
+
 			var the = document.createElement('td');
-			the.className = "clicked";
-		
+			
 			the.textContent = data[j][columns[i]];
+			// console.log(data[j][columns[i]]);
 			tre.appendChild(the);
-	
+			// console.log(tre.appendChild(the));
+		
+		}
+		tbody.appendChild(tre);
+
+		//Own code for Historik
+		if(columns[i] === "Historik" && data[j][columns[i]] !== undefined) {
+			
+			for (var k = 0; k < d.length; k++)  {
+				var div = "<div></div>";
+				
+				var subtr = document.createElement('tr');
+				subtr.id = "hidden" + j;
+				
+
+				// subtr.style = 'display: none';
+				
+				
+				// console.log(d[k][columns[i]]);
+				for(var z = 0; z < columns.length; z++) {
+					// console.log(d[k][columns[z]]);
+					var sub = document.createElement('td');
+					
+					sub.textContent = d[k][columns[z]];
+					sub.id = 'sub' + j;
+
+					subtr.appendChild(sub);
+					
+			}
+				
+					
+				tbody.appendChild(subtr);
+
+			}
+		
+			
 		}
 		
-		tbody.appendChild(tre);
+		
 	};
+	
+
 	// emptyDOMChildren(sel);
 	sel.appendChild(tbe);
-	$(".clicked").click(function() {
-		// console.log(data);
-		for (var i = 0; i < data.length; i++){
-			// console.log(data[i]["Historik"]);
-			if(data[i]["Historik"] !== undefined){
-			var hist = data[i]["Historik"];
-				for(var j = 0; j < hist.length; j++){
-					if(hist[j]["Lokation"] === data[i]["Lokation"]){
-						console.log(data[i]["Historik"][j].Lokation);
-					}
-				
-				}
+
+$("td").show(function(){
+	var hid = "hidden";
+	var retval = [];
+	$("td").each(function(){
+		var Id = $(this).attr('id');
+		if(Id !== undefined){
+		retval.push(Id);
+			
+		}
+		for(var i = 0; i < retval.length; i++){
+			if($(this).attr('id') == retval[i]){
+				$(this).hide();
+			}
+			
+		}
+		
+	})
+console.log(retval);
+
+	
+});
+$("tr").click(function(){
+	$("td").each(function(){
+		var retval = [];
+
+		var Id = $(this).attr('id');
+		if(Id !== undefined){
+		retval.push(Id);
+			
+		}
+		var Id = $(this).attr('id');
+		for(var i = 0; i < retval.length; i++){
+			if($(this).attr('id') == retval[i]){
+				$(this).show();
 			}
 		}
-	});
+	})
+});
+
 };
-
-
