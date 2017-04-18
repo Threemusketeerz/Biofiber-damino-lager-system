8d7e0d7bbd46f65aabdcb9170434b97ce64ec917
Sporring55 <bj@damino02.local>
2017-04-18 09:54:11 +0200

    Search function working
    TH hides it self needs a bug fix

--- scripts/search.js
+++ scripts/search.js
@@ -9,14 +9,12 @@ $(document).ready(function(){
                 td = document.getElementsByTagName("td");
                 console.log(filter);
             for(var i = 0; i < tr.length; i++){
-                console.log(tr[i]);
+          
                 a = tr[i];
-                if(a.innerHTML.toUpperCase().indexOf(filter) > - 1){
+                if(a.innerHTML.toUpperCase().indexOf(filter) > - 1 && !tr[i].id){
                     tr[i].style.display = "";
 
-                }else if (a.innerHTML.toUpperCase().indexOf(filter) < -1){
-                    tr[i].style.display = "none";
-                }else if(tr[i].style.display = "none"){
+                }else{
                     tr[i].style.display = "none";
                 }
             }
